3ccacce46c1965c46b6e452845120de6ff3b58fb
Florian Celestin <F.Celestin@epyx.co.uk>
2019-08-16 11:46:37 +0100

    8

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, NavLink, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import Courses from './containers/Courses/Courses';
 import Users from './containers/Users/Users';
@@ -28,6 +28,7 @@ class App extends Component {
             <Route path="/users" exact component={Users} ></Route>
             <Route path="/courses" component={Courses} ></Route>
             <Route path="/" exact component={Users} ></Route>
+            <Redirect from='/all-courses' to='/courses'></Redirect>
             <Route render={() => <h1>Not Found</h1>}></Route>
           </Switch>
         </div>
